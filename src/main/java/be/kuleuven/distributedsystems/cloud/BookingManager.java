44b9995fda52ed0bfa2c6fc27a482ba27fb967c8
Nicolas Scheers <nicolas.scheers@student.kuleuven.be>
2022-11-24 15:41:51 +0100

    firestore

--- src/main/java/be/kuleuven/distributedsystems/cloud/BookingManager.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/BookingManager.java
@@ -4,8 +4,14 @@ import be.kuleuven.distributedsystems.cloud.entities.Booking;
 import be.kuleuven.distributedsystems.cloud.entities.Quote;
 import be.kuleuven.distributedsystems.cloud.entities.Ticket;
 import be.kuleuven.distributedsystems.cloud.entities.User;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.v1.FirestoreClient;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.awt.print.Book;
+import javax.annotation.Resource;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +22,23 @@ public class BookingManager {
     List<Booking> bookings;
 
 
-    public BookingManager() {
+    @Autowired
+    private Firestore getFirestore;
+
+
+
+    /*
+    @PostConstruct
+    void constructor(){
+        this.db.collection("tickets").get();
+    }
+
+     */
+
+
+
+
+    public BookingManager()  {
         this.customers = new ArrayList<User>();
         this.bookings = new ArrayList<Booking>();
     }
@@ -43,7 +65,17 @@ public class BookingManager {
     }
 
     public List<Booking> getBookings() {
-        return bookings;
+        List<Booking> bookingList = new ArrayList<>();
+        try {
+            List<QueryDocumentSnapshot> documents = getFirestore.collection("bookings").get().get().getDocuments();
+            for (DocumentSnapshot document : documents) {
+                bookingList.add(document.toObject(Booking.class));
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return  bookingList;
     }
 
     public void setBookings(List<Booking> bookings) {
@@ -51,6 +83,7 @@ public class BookingManager {
     }
 
     public void addBooking(Booking booking){
+        getFirestore.collection("bookings").add(booking);
         this.bookings.add(booking);
     }
 }
--- src/main/java/be/kuleuven/distributedsystems/cloud/controller/BookingController.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/controller/BookingController.java
@@ -2,18 +2,12 @@ package be.kuleuven.distributedsystems.cloud.controller;
 
 import be.kuleuven.distributedsystems.cloud.BookingManager;
 import be.kuleuven.distributedsystems.cloud.WebService;
-import be.kuleuven.distributedsystems.cloud.WebServiceImpl;
 import be.kuleuven.distributedsystems.cloud.entities.*;
-import com.google.cloud.firestore.Firestore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 
 @RestController
@@ -24,15 +18,9 @@ public class BookingController {
     private BookingManager bookingManager = new BookingManager();
 
     private static String apiKey = "Iw8zeveVyaPNWonPNaU0213uw3g6Ei";
-/*
-    @Resource(name = "db")
-    private Firestore db;
 
-    @PostConstruct
-    void constructor(){
-        this.db.collection("tickets").get();
-    }
-*/
+
+
     @GetMapping("getBookings")
     public Booking[] getBookings(){
         //Todo: Firestore logica
